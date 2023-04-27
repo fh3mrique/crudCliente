6eb06d64aca28f607eb307a10614f3be4292f7f5
fh3mrique <97576330+fh3mrique@users.noreply.github.com>
2023-04-27 19:24:58 -0300

    Criando projeto

---
+++ backendClienteCrud/src/main/java/com/pessoalProjeto/clienteCrud/ClienteCrudApplication.java
@@ -0,0 +1,13 @@
+package com.pessoalProjeto.clienteCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteCrudApplication.class, args);
+	}
+
+}
