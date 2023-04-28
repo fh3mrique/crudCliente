83861e48d1e7724868cb09a07efc85f981febb54
fh3mrique <97576330+fh3mrique@users.noreply.github.com>
2023-04-28 16:16:53 -0300

    Método DELETE, Deletando Cliente

--- backendClienteCrud/src/main/java/com/pessoalProjeto/clienteCrud/services/ClienteService.java
+++ backendClienteCrud/src/main/java/com/pessoalProjeto/clienteCrud/services/ClienteService.java
@@ -7,81 +7,88 @@ import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.pessoalProjeto.clienteCrud.dto.ClienteDTO;
 import com.pessoalProjeto.clienteCrud.entities.Cliente;
 import com.pessoalProjeto.clienteCrud.repositories.ClienteRepository;
+import com.pessoalProjeto.clienteCrud.services.exceptions.BancoDeDadoException;
 import com.pessoalProjeto.clienteCrud.services.exceptions.EntidadeNaoEncontrada;
 
 @Service
 public class ClienteService {
-	
+
 	@Autowired
 	private ClienteRepository repository;
-	
+
 	@Transactional(readOnly = true)
-	public List<ClienteDTO> findAll(){
-		
+	public List<ClienteDTO> findAll() {
+
 		List<Cliente> listaCliente = repository.findAll();
-		
+
 		List<ClienteDTO> listadto = new ArrayList<>();
-		
+
 		for (Cliente cat : listaCliente) {
 			listadto.add(new ClienteDTO(cat));
 		}
-		
+
 		return listadto;
-		
-		
+
 	}
 
 	@Transactional(readOnly = true)
 	public ClienteDTO findById(Long id) {
-		
+
 		Optional<Cliente> obj = repository.findById(id);
-		
+
 		Cliente entidade = obj.orElseThrow(() -> new EntidadeNaoEncontrada("Entidade não encontrada"));
-		
+
 		return new ClienteDTO(entidade);
 	}
 
 	@Transactional
 	public ClienteDTO insert(ClienteDTO dto) {
-		
+
 		Cliente entidade = new Cliente();
-		
-//		entidade.setName(dto.getName());
-//		entidade.setBirthDate(dto.getBirthDate());
-//		entidade.setCpf(dto.getCpf());
-//		entidade.setIncome(dto.getIncome());
-//		entidade.setChildren(dto.getChildren());
-		
+
 		CopiarDtoParaEntidade(entidade, dto);
-		
+
 		entidade = repository.save(entidade);
-		
-		
+
 		return new ClienteDTO(entidade);
 	}
 
 	@Transactional
 	public ClienteDTO update(Long id, ClienteDTO dto) {
-		
 		try {
-			
+
 			Cliente entidade = repository.getOne(id);
-			
+
 			CopiarDtoParaEntidade(entidade, dto);
-			
+
 			entidade = repository.save(entidade);
-			
+
 			return new ClienteDTO(entidade);
 		}
-		
+
 		catch (EntityNotFoundException e) {
 			throw new EntidadeNaoEncontrada("id não encontrado");
+		}
+	}
+
+	public void delete(Long id) {
+		
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new EntidadeNaoEncontrada("id não encontrado");
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new BancoDeDadoException("violação na integridade do banco");
 		}
 	}
 
@@ -91,8 +98,7 @@ public class ClienteService {
 		entidade.setIncome(dto.getIncome());
 		entidade.setBirthDate(dto.getBirthDate());
 		entidade.setChildren(dto.getChildren());
-			
+
 	}
-	
-	
+
 }
---
+++ backendClienteCrud/src/main/java/com/pessoalProjeto/clienteCrud/services/exceptions/BancoDeDadoException.java
@@ -0,0 +1,12 @@
+package com.pessoalProjeto.clienteCrud.services.exceptions;
+
+public class BancoDeDadoException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	public BancoDeDadoException(String msg) {
+		super(msg);
+	}
+
+}
